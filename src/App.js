d1c9a9361de749cd67085f8a0c34022c72f2cef7
OleksiiRomanchenko <oleksii.romanchenko@itechart-group.com>
2021-10-03 15:48:30 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,32 +2,31 @@ import React, { useState, useEffect, useLayoutEffect, useRef } from 'react';
 
 export default function App() {
   const [counter, setCounter] = useState(0);
-  const counterRef = useRef(0);
+  // const counterRef = useRef(0);
 
   useEffect(() => {
     console.log('useEffect', counter);
     setCounter(4);
 
-    counterRef.current++;
+    //counterRef.current++;
   }, [counter]);
 
   useLayoutEffect(() => {
     console.log('useLayoutEffect', counter);
-    setCounter(3);
+    setCounter(5);
 
-    counterRef.current++;
+    //counterRef.current++;
   });
 
   const onClickButton = () => {
     let newCounter = counter + 1;
-    //setCounter(newCounter);
+    setCounter(newCounter);
 
-    counterRef.current++;
+    //counterRef.current++;
   };
 
   return (
     <div>
-      <h1>{counterRef.current}</h1>
       <button onClick={onClickButton}>Click btn</button>
     </div>
   );
